32f1cdc6c0dabc46044cd2d52609903c2f6b1092
Ian Matic <lordmatic@yahoo.com>
2019-03-16 20:29:05 -0400

    Working on menu and loading data
    Signed-off-by: Ian Matic <lordmatic@yahoo.com>

---
+++ Deluge/Assets/Scripts/Loading/GameData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//static class is used to handle data between scenes
+public static class GameData
+{
+    private static string targetScene;
+
+    public static string TargetScene
+    {
+        get { return targetScene; }
+        set { targetScene = value; }
+    }
+}
---
+++ Deluge/Assets/Scripts/Loading/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    //specify in Unity
+    public string targetScene;
+    public bool needLoadScreen;
+
+    public void LoadScene()
+    {
+
+        if (needLoadScreen)
+        {
+            //load the load screen and pass through the target scene
+            SceneManager.LoadScene("LoadScene");
+            GameData.TargetScene = targetScene;
+        }
+        else
+        {
+            //load the scene
+            SceneManager.LoadScene(targetScene);
+        }
+    }
+}
