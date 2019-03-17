85f1bc92313327759c1cdf1c4683009868050b2d
Ian Matic <lordmatic@yahoo.com>
2019-03-16 22:54:09 -0400

    Implemented basic menu

---
+++ Deluge/Assets/Scripts/Loading/LoadingTimer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingTimer : MonoBehaviour
+{
+    public float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 1.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+
+        //transition to game when timer reaches 0
+        if (time < 0)
+        {
+            SceneManager.LoadScene(GameData.TargetScene);
+        }
+    }
+}
